fd319fb95ce8b9de1d34ce52c8c653b4531d6f38
Emily Dokken <dokkenemily@gmail.com>
2022-02-13 15:45:36 -0800

    create login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,21 @@ router.post('/', (req, res) => {
     });
 });
 
+// POST User login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        res.json({ user: dbUserData });
+    });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     User.update(req.body, {
