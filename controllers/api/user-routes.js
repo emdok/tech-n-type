a766a4ea948a6bf1525ec8fd295f85092d893ce8
Emily Dokken <dokkenemily@gmail.com>
2022-02-13 20:08:53 -0800

    add sessions for users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,10 +55,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        res.status(500).json(err);
-    });
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
 });
 
 // POST User login
@@ -75,12 +80,19 @@ router.post('/login', (req, res) => {
 
         //Verify User
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect password! '});
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in! '});
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in! '});
+        });
     });
 });
 
