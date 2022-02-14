72c248df97428243efd238e0cf11f082782fb3b2
Emily Dokken <dokkenemily@gmail.com>
2022-02-13 16:59:10 -0800

    add the ability to get a single post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
 
-// get all users
+// get all posts by users
 router.get('/', (req, res) => {
     console.log('=====================');
     Post.findAll({ 
@@ -14,6 +14,33 @@ router.get('/', (req, res) => {
         ]
     })
     .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//get a single post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post dound with this id '});
+            return;
+        }
+        res.json(dbPostData);
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
