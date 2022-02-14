c514c390dff907a1af931fb5221e99a59badd7ca
Emily Dokken <dokkenemily@gmail.com>
2022-02-13 16:29:04 -0800

    establish table relationships in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require("./Post");
 
+//User has many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//Post belong to users
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
